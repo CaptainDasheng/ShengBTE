1f89be746b3209098d2739e2efc01e6e042f02ca
Malkino <lz3an@abv.bg>
2016-09-23 21:46:49 +0100

    Add files via upload

--- thirdorder_castep.py
+++ thirdorder_castep.py
@@ -60,7 +60,134 @@ sys.path.insert(0, '../')
 import thirdorder_core
 from thirdorder_common import *
 
+symbol_map = {
+    "H":1,
+    "He":2,
+    "Li":3,
+    "Be":4,
+    "B":5,
+    "C":6,
+    "N":7,
+    "O":8,
+    "F":9,
+    "Ne":10,
+    "Na":11,
+    "Mg":12,
+    "Al":13,
+    "Si":14,
+    "P":15,
+    "S":16,
+    "Cl":17,
+    "Ar":18,
+    "K":19,
+    "Ca":20,
+    "Sc":21,
+    "Ti":22,
+    "V":23,
+    "Cr":24,
+    "Mn":25,
+    "Fe":26,
+    "Co":27,
+    "Ni":28,
+    "Cu":29,
+    "Zn":30,
+    "Ga":31,
+    "Ge":32,
+    "As":33,
+    "Se":34,
+    "Br":35,
+    "Kr":36,
+    "Rb":37,
+    "Sr":38,
+    "Y":39,
+    "Zr":40,
+    "Nb":41,
+    "Mo":42,
+    "Tc":43,
+    "Ru":44,
+    "Rh":45,
+    "Pd":46,
+    "Ag":47,
+    "Cd":48,
+    "In":49,
+    "Sn":50,
+    "Sb":51,
+    "Te":52,
+    "I":53,
+    "Xe":54,
+    "Cs":55,
+    "Ba":56,
+    "La":57,
+    "Ce":58,
+    "Pr":59,
+    "Nd":60,
+    "Pm":61,
+    "Sm":62,
+    "Eu":63,
+    "Gd":64,
+    "Tb":65,
+    "Dy":66,
+    "Ho":67,
+    "Er":68,
+    "Tm":69,
+    "Yb":70,
+    "Lu":71,
+    "Hf":72,
+    "Ta":73,
+    "W":74,
+    "Re":75,
+    "Os":76,
+    "Ir":77,
+    "Pt":78,
+    "Au":79,
+    "Hg":80,
+    "Tl":81,
+    "Pb":82,
+    "Bi":83,
+    "Po":84,
+    "At":85,
+    "Rn":86,
+    "Fr":87,
+    "Ra":88,
+    "Ac":89,
+    "Th":90,
+    "Pa":91,
+    "U":92,
+    "Np":93,
+    "Pu":94,
+    "Am":95,
+    "Cm":96,
+    "Bk":97,
+    "Cf":98,
+    "Es":99,
+    "Fm":100,
+    "Md":101,
+    "No":102,
+    "Lr":103,
+    "Rf":104,
+    "Db":105,
+    "Sg":106,
+    "Bh":107,
+    "Hs":108,
+    "Mt":109,
+    "Ds":110,
+    "Rg":111,
+    "Cn":112,
+    "Uut":113,
+    "Uuq":114,
+    "Uup":115,
+    "Uuh":116,
+    "Uus":117,
+    "Uuo":118,
+    }
 
+
+
+
+def sort_elements(elem):
+    
+    return symbol_map[elem[0]]
+      
 # Convert this to castep. Will use a cell file for the moment
 
 ########################################################################
@@ -82,7 +209,7 @@ def read_POSCAR(directory):
               for i in xrange(3):
                   new_line = index + i + 1
                   nruter["lattvec"][:,i]=[float(j) for j in castep_cell[new_line].split()]
-           #elif '%BLOCK lattice_abc' in line: # add this part later
+
            
            elif '%BLOCK positions_frac' in line:
                 index_start = index
@@ -93,22 +220,20 @@ def read_POSCAR(directory):
         
         nruter["lattvec"]*=0.1
         for i in range(index_start+1, index_end):
-               atoms_list.append(castep_cell[i].split())
-        atoms_list = filter(None,atoms_list)
-        atoms_list.sort(key=lambda tup: tup[0]) 
+               atoms_list.append(castep_cell[i].split())      
+	atoms_list = filter(None,atoms_list)
+        atoms_list.sort(key=sort_elements) 
         natoms1 = len(atoms_list)
         nruter["positions"]=np.empty((3,natoms1))
         for i in range(natoms1):          
             nruter["positions"][:,i]=[float(atoms_list[i][j]) for j in xrange(1,4)]
-            nruter["elements"].append(str(atoms_list[i][0]))        
-        nruter["elements"].sort(key=lambda tup: tup[0])
-        test = nruter["elements"]
+            nruter["elements"].append(str(atoms_list[i][0]))
+        create_indices = nruter["elements"]
         nruter["elements"]=list(set(nruter["elements"]))
-        nruter["elements"].sort()
-        nruter["numbers"]= np.array([int(test.count(nruter["elements"][i])) for i in range(len(nruter["elements"]))],dtype=np.intc)  
+        nruter["numbers"]= np.array([int(create_indices.count(nruter["elements"][i])) for i in range(len(nruter["elements"]))],dtype=np.intc)  
         nruter["types"]=[]
         for i in xrange(len(nruter["numbers"])):
-             nruter["types"]+=[i]*nruter["numbers"][i]    
+             nruter["types"]+=[i]*nruter["numbers"][i]  
     return nruter
 
 def write_POSCAR(poscar,filename):
